ef2e144853a67f104df398f1997749f262f1ddf0
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-09-11 12:34:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,63 @@
 #Don't Remove Credit @Hgbotz 
+
+import aiohttp
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+# ===== CONFIG =====
+API_ID = 
+API_HASH = "  "
+BOT_TOKEN = "  "
+
+
+# ===== BOT INSTANCE =====
+client = Client("ott_scraper_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# ===== INLINE BUTTON =====
+update_button = InlineKeyboardMarkup(
+    [[InlineKeyboardButton("ðŸ˜¶â€ðŸŒ«ï¸ Updates", url ="https://t.me/hgbotz")]]
+)
+
+# ===== COMMON FUNCTION =====
+async def fetch_ott_data(api_url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as resp:
+            if resp.status != 200:
+                return None
+            return await resp.json()
+
+async def handle_ott_command(message: Message, api_url: str):
+    msg = await message.reply("ðŸ” Fetching...")
+    try:
+        data = await fetch_ott_data(api_url)
+        if not data:
+            return await msg.edit_text("âŒ Failed to fetch data from API.")
+
+        title = data.get("title") or "No Title"
+        image_url = data.get("poster") or data.get("landscape")
+
+        if not title and not image_url:
+            return await msg.edit_text("âš ï¸ No title or poster found for this URL.")
+            
+            #Don't Remove Credit @Hgbotz 
+
+        text = (
+            f"ðŸŽ¬ <b>{title}</b>\n\n"
+            f"ðŸ–¼ï¸ Poster: {image_url}\n\n"
+            
+            "<b><blockquote>Powered By <a href='https://t.me/hgbotz'>ð™·ð™¶ð™±ð™¾ðšƒá¶» ðŸ¦‹</a></blockquote></b>"
+        )
+
+        await msg.edit_text(
+            text=text,
+            disable_web_page_preview=False,
+            reply_markup=update_button
+        )
+
+    except Exception as e:
+        await msg.edit_text(f"âŒ Error: {e}")
+
+#Don't Remove Credit @Hgbotz 
 # ===== COMMAND HANDLERS =====
 @client.on_message(filters.command(["sunnext", "hulu", "stage", "adda", "wetv", "plex", "iqiyi", "aha", "shemaroo", "apple"]))
 async def ott_cmd(client, message: Message):
